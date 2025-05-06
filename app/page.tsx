6f4aade1b76618b9c17161a9bd69174d83be22b9
ARNAGUTI <aitormargon16@gmail.com>
2025-05-06 10:46:14 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Chat from "@/components/chat"; // minúscula porque el archivo es "chat.tsx"
+import { Chat } from "@/components/chat"; // minúscula porque el archivo es "chat.tsx"
 
 export default function HomePage() {
   return (
