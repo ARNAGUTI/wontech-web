342d0db748236df0d66966aee0b76a6a59d75c7a
ARNAGUTI <aitormargon16@gmail.com>
2025-05-14 11:01:36 +0200

    page.tsx

--- app/(chat)/embed/page.tsx
+++ app/(chat)/embed/page.tsx
@@ -1,5 +1,5 @@
 import { cookies } from 'next/headers';
-import { Chat } from '@/components/chat';
+import Chat from '@/components/chat';
 import { DEFAULT_CHAT_MODEL } from '@/lib/ai/models';
 import { generateUUID } from '@/lib/utils';
 import { DataStreamHandler } from '@/components/data-stream-handler';
