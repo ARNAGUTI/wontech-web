065259df123fce4e78490972fcb6452ac6461d0c
ARNAGUTI <aitormargon16@gmail.com>
2025-05-15 12:49:55 +0200

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,16 @@
-"use client";
-import Link from "next/link";
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <div className="text-2xl font-bold">
-        <Link href="/">Wontech Web</Link>
-      </div>
-      <div className="flex gap-4">
-        <Link href="/page2" className="hover:text-gray-400">
+    <nav className="bg-gray-800 text-white py-4 px-8 flex justify-between items-center shadow-lg">
+      <Link href="/" className="text-2xl font-bold hover:text-gray-400 transition duration-200">
+        Wontech Web
+      </Link>
+      <div className="flex gap-8">
+        <Link href="/page2" className="text-lg hover:text-gray-400 transition duration-200">
           Página 2
         </Link>
-        <Link href="/page3" className="hover:text-gray-400">
+        <Link href="/page3" className="text-lg hover:text-gray-400 transition duration-200">
           Página 3
         </Link>
       </div>
