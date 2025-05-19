d4a10600fd0b557f94827c0eeedc6a86003b62d4
ARNAGUTI <aitormargon16@gmail.com>
2025-05-19 10:29:20 +0200

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,25 +1,27 @@
 import Link from 'next/link';
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+    <nav className="w-full bg-gray-900 text-white py-3 px-8 flex items-center fixed top-0 z-50">
       <div className="flex-1">
-        <Link href="/" className="text-2xl font-bold">
+        <Link href="/" className="text-2xl font-bold hover:text-gray-400 transition">
           Wontech Web
         </Link>
       </div>
-      <div className="flex-1 flex justify-center">
-        <Link href="/page2" className="mx-4">
+
+      <div className="flex-1 flex justify-center text-lg">
+        <Link href="/page2" className="hover:text-gray-400 transition">
           Página 2
         </Link>
       </div>
-      <div className="flex-1 flex justify-end">
-        <Link href="/page3" className="mx-4">
+
+      <div className="flex-1 flex justify-end text-lg">
+        <Link href="/page3" className="hover:text-gray-400 transition">
           Página 3
         </Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
